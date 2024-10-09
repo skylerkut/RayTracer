97da6ec1bda9df49f9391634d39c54e7a881fe36
skutsch <skutsch@iastate.edu>
2024-10-09 13:57:59 -0500

    added render progress reporting

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -28,7 +28,26 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-}
+
+    //Render loop with progress reporting
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+}//end main
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
