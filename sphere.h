7ba0ec505d94c2be9575f1a2791720e12ab95fb5
skutsch <skutsch@iastate.edu>
2024-10-27 20:09:47 -0500

    6.7

---
+++ sphere.h
@@ -0,0 +1,41 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+public:
+	sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+	boot hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc); //h = quadratic equation sol.
+        auto c = oc.length_squared() - radius * radius;
+        auto discriminant = h * h - a * c;
+
+        if (discriminant < 0) {
+            return false;
+        }
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        //Find nearest node in acceptable range
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - center) / radius;
+
+        return true;
+    }
+private: 
+    point3 center;
+    double radius;
+};
+#endif
