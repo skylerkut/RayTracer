890bf4bfd317d0a31808fb3c27e02f255a94a835
skutsch <skutsch@iastate.edu>
2024-11-19 08:44:24 -0600

    added interval.h

--- sphere.h
+++ sphere.h
@@ -5,7 +5,7 @@ class sphere : public hittable {
 public:
 	sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
-	bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc); //h = quadratic equation sol.
@@ -20,9 +20,9 @@ public:
 
         //Find nearest node in acceptable range
         auto root = (h - sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root) {
+        if (!ray_t.surrounds(root)) {
             root = (h + sqrtd) / a;
-            if (root <= ray_tmin || ray_tmax <= root)
+            if (!ray_t.surrounds(root))
                 return false;
         }
 
