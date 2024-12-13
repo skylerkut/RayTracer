10447201fb12b9171780b2eb2758ca0d2f30fb6a
skutsch <skutsch@iastate.edu>
2024-12-13 14:31:14 -0600

    camera fov

--- vec3.h
+++ vec3.h
@@ -125,6 +125,13 @@ public:
 		  return v - 2 * dot(v, n) * n;
 	  }
 
+	  inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+		  auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+		  vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+		  vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+		  return r_out_perp + r_out_parallel;
+	  }
+
 
 #endif
 
